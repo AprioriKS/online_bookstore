b138782f2709b170f3c2c4b3a40592fc7fc340bf
ghdfjhgdfjhgjdg <sounddummies5@gmail.com>
2024-09-04 15:39:02 +0300

    initial commiat

---
+++ src/main/java/com/example/online_bookstore_my/OnlineBookstoreMyApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_bookstore_my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookstoreMyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OnlineBookstoreMyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/online_bookstore_my/OnlineBookstoreMyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_bookstore_my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreMyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
