75d4c0653adc127c58570f4fef4fb2b2d0dccbd7
Kostya Bugaiov <sounddummies@gmail.com>
2024-10-07 18:51:03 +0300

    Update src/main/java/com/example/onlinebookstoremy/bookstore/service/UserServiceImpl.java
    Co-authored-by: Liliia Ponomarenko <96740815+liliia-ponomarenko@users.noreply.github.com>

--- src/main/java/com/example/onlinebookstoremy/bookstore/service/UserServiceImpl.java
+++ src/main/java/com/example/onlinebookstoremy/bookstore/service/UserServiceImpl.java
@@ -25,7 +25,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserResponseDto register(UserRegistrationRequestDto userRegistrationRequestDto) {
         if (userRepository.existsByEmail(userRegistrationRequestDto.getEmail())) {
-            throw new RegistrationException("User already exists");
+            throw new RegistrationException("User with email: " + email + "already exists");
         }
         User user = userMapper.toModel(userRegistrationRequestDto);
         user.setPassword(passwordEncoder.encode(userRegistrationRequestDto.getPassword()));
